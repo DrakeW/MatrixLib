38d7c5dc558380969533ab5ea35b98d8c495fb8a
DrakeW <126422204+DrakeW@users.noreply.github.com>
2024-03-28 15:22:51 -0700

    A bit more testing to make sure that everything works

--- MatrixClient/MatrixClient.cpp
+++ MatrixClient/MatrixClient.cpp
@@ -1,13 +1,13 @@
 #include "Matrix.h"
-#include "MatrixClassDef.cpp"
 #include <iostream>
+#include "MatrixClassDef.cpp"
 
 int main()
 {
 	mtx::matrix<int> Matrix(5, 5, 1);
 	std::cout << mtx::IntMatrixToString(Matrix) << std::endl;
-	std::vector<std::vector<int>> Fill = { {2, 2}, {3, 3} };
-	if(Matrix.FillData(Fill, 2, 1))
+	std::vector<int> Fill = {3, 3};
+	if(Matrix.InsertHorazontalListData(Fill, 1, 1))
 		std::cout << mtx::IntMatrixToString(Matrix) << std::endl;
 	return 0;
 }
