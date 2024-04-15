0e5299149a964f729afe02ec965aa6007b168c2f
Drake Wright <126422204+DrakeW@users.noreply.github.com>
2024-04-15 11:06:25 -0700

    Multiply function fixed

--- MatrixClient/MatrixClient.cpp
+++ MatrixClient/MatrixClient.cpp
@@ -16,15 +16,10 @@ int main()
 {
 	mtx::ErrorFunc = &NewPrint;
 
-<<<<<<< Updated upstream
 	mtx::matrix<int> Matrix(2, 3, 0);
 	mtx::matrix<int> SecondMatrix(3, 2, 0);
 	mtx::print(IntMatrixToString(Matrix));
 	mtx::print(IntMatrixToString(SecondMatrix));
-=======
-	mtx::matrix<int> Matrix(4, 2, 0);
-	mtx::matrix<int> SecondMatrix(3, 3, 0);
->>>>>>> Stashed changes
 	mtx::print(mtx::IntMatrixToString(mtx::Multiply(Matrix, SecondMatrix)));
 	
 	return 0;
