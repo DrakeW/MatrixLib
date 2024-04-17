8f7a68b1b69e9c5058fdca7338b7396832309cab
DrakeW <126422204+DrakeW@users.noreply.github.com>
2024-04-17 13:36:40 -0700

    Removed some testing functions

--- MatrixClient/MatrixClient.cpp
+++ MatrixClient/MatrixClient.cpp
@@ -2,19 +2,8 @@
 #include <string>
 #include "MatrixClassDef.cpp"
 
-int ToTwo(int x)
-{
-	return 2;
-}
-
-void NewPrint(std::string input)
-{
-	std::cout << "{Yay! this thing is working!} " << input << std::endl;
-}
-
 int main()
 {
-	mtx::ErrorFunc = &NewPrint;
 
 	mtx::matrix<bool> Matrix(2, 3, true);
 	mtx::matrix<bool> SecondMatrix(3, 2, true);
